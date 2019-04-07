436d27843f7792106f50ab5f308480a08ab691b1
Steven1324 <2232599022@qq.com>
2019-04-07 19:22:27 +0800

    hhj上传
    上传

---
+++ js/head.js
@@ -0,0 +1,12 @@
+$(function(){
+// 划上显示效果
+    $('.U-one').hover(function(e){
+		$('.one').stop(true).slideToggle();
+	});
+	$('.U-two').hover(function(e){
+		$('.two').stop(true).slideToggle();
+	});
+	$('.U-three').hover(function(e){
+		$('.three').stop(true).slideToggle();
+	})
+})
