c4e81e2053602a006b221ba723da154d23d344d5
mengnaL <3328802744@qq.com>
2019-04-09 12:33:37 +0800

    首页 尾页
    添加与改正

--- js/momoServe.js
+++ js/momoServe.js
@@ -1,3 +1,18 @@
+// var screenYY=window.screenY;//FireFox相对于屏幕的y坐标
+//   //event事件问题
+//   document.onclick=function(ev){//谷歌火狐的写法，IE9以上支持，往下不支持；
+//     var e=ev;
+//     console.log(e);
+//   }
+//   document.onclick=function(){//谷歌和IE支持，火狐不支持；
+//     var e=event;
+//     console.log(e);
+//   }
+//   document.onclick=function(ev){//兼容写法；
+//     var e=ev||window.event;
+//     var mouseX=e.clientX;//鼠标X轴的坐标
+//     var mouseY=e.clientY;//鼠标Y轴的坐标
+//   }
 $(function(){
 // 划上显示效果
     $('.U-one').hover(function(e){
@@ -65,10 +80,15 @@ $(function(){
 
 // 帮助中心
 	$('#B-ul li').click(function(){
-		var index = $(this).index();
-		$('#B-ul li img').eq(index).addClass("action").siblings().removeClass("action");
-		console.log($(this),index)
-		$('#B-ul li span').eq(index).addClass("active").siblings('#B-ul li span').removeClass("active");
-		$('#B-search div').eq(index).show().siblings('#B-search div').hide();
+		$('#B-ul li').click(function(){
+			var index = $(this).index();
+			$('#B-ul li img').eq(index).addClass("action");
+			$('#B-ul li span').eq(index).addClass("active");
+			$('#B-search div').eq(index).show().siblings('#B-search div').hide();
+			$('#B-ul li').click(function(){
+				$('#B-ul li img').eq(index).removeClass("action");
+				$('#B-ul li span').eq(index).removeClass("active");
+			})
+		})
 	})
 })
--- js/shouyeindex.js
+++ js/shouyeindex.js
@@ -1,16 +1,20 @@
-$(function(){
+$(function(){  
     // 划上显示效果
-        $('.U-one').hover(function(e){
-            $('.one').stop(true).slideToggle();
-        });
-        $('.U-two').hover(function(e){
-            $('.two').stop(true).slideToggle();
-        });
-        $('.U-three').hover(function(e){
-            $('.three').stop(true).slideToggle();
-        });
-        // $(".photoWall2 .li2").on('hover',function(){
-        //     $(".photoWall2 .li2 span").css({"top":0,"transition":"1.5s"})
-        //     console.log($(this).index())
-        // });
+    $('.U-one').hover(function(e){
+        $('.one').stop(true).slideToggle();
     });
+    $('.U-two').hover(function(e){
+        $('.two').stop(true).slideToggle();
+    });
+    $('.U-three').hover(function(e){
+        $('.three').stop(true).slideToggle();
+    })
+      
+    // 视频
+    $('#play').click(function(){
+        $('#video').css({"display":"block"});
+    })
+    $('#vedio_img').click(function(){
+        $('#video').css({"display":"none"});
+    })
+});
